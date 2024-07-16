a2cce4a370e04882e6324cda61a4ba64305076e1
AdarshKumarKadam <adarshkadam07@gmail.com>
2024-07-16 12:40:58 +0530

    git commit -m "updated user slice url "

--- client/src/slices/usersApiSlice.js
+++ client/src/slices/usersApiSlice.js
@@ -1,6 +1,6 @@
 import { apiSlice } from './apiSlice';
 
-const USERS_URL = 'http://localhost:3000/api/user';
+const USERS_URL = 'https://mern-authentication-rtk.vercel.app/api/user';
 
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
