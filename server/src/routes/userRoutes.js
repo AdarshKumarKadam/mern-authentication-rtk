c92048806e4a3e16d3ccc447e3fbc820d702d6d9
AdarshKumarKadam <adarshkadam07@gmail.com>
2024-07-21 16:01:25 +0530

    Resolved conflicts and applied stashed changes

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -10,49 +10,41 @@ const authenticate = require("../middlewares/authMiddleware");
 const roles = require("../utility/roles");
 
 // Register user
-router.post("/", async (req, res, next) => {
-  const newUser = req.body;
+router.post(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const newUser = req.body;
 
-  try {
-    let user = await User.findOne({ email: newUser.email });
+    const user = await User.findOne({ email: newUser.email });
 
     if (user) {
-      return res.status(400).json({ message: "User already exists !!" });
+      return res.status(400).json({ message: "User with email already exists !!" });
     }
 
     const hashedPassword = await bcrypt.hash(newUser.password, 13);
     newUser.password = hashedPassword;
 
-    user = new User(newUser);
-    user.role = roles.USER;
-    const userA = await user.save();
+    const newUserInstance = new User(newUser);
+    newUserInstance.role = roles.USER;
+    const savedUser = await newUserInstance.save();
 
     res.status(201).json({
       message: "User registered successfully !!",
       data: {
-        _id: userA._id,
-        name: userA.name,
-        email: userA.email,
-        role: userA.role,
+        _id: savedUser._id,
+        name: savedUser.name,
+        email: savedUser.email,
+        role: savedUser.role,
       },
     });
-  } catch (error) {
-    console.log(error);
-    res
-      .status(500)
-      .json({ message: "Failed to register user!", error: error.message });
-  }
-});
+  })
+);
 
 // User login
 router.post(
   "/login",
   asyncHandler(async (req, res, next) => {
-    console.log("login api called !! ");
-
     const { email, password } = req.body;
-
-    console.log("Login :" + email, password);
 
     const user = await User.findOne({ email });
 
@@ -70,101 +62,100 @@ router.post(
         role: user.role || "user",
       });
     } else {
-      res.status(400);
-      throw new Error("Invalid email or password !");
+      res.status(400).json({ message: "Invalid email or password!" });
     }
   })
 );
 
-//forgot  password
-router.post("/forgotPassword", async (req, res, next) => {
-  const { email } = req.body;
-  try {
-    console.log(email);
+// Forgot password
+router.post(
+  "/forgotPassword",
+  asyncHandler(async (req, res, next) => {
+    const { email } = req.body;
+
     const user = await User.findOne({ email });
+
     if (!user) {
-      res.status(404);
-      throw new Error("User not found");
+      return res.status(404).json({ message: "User not found" });
     }
+
     const otp = otpHandler.generateOTP();
-    console.log(otp);
     otpHandler.storeOTP(email, otp, 10);
     await sendMail(email, otp);
 
-    res.status(200).json({ message: "Reset password email sent !" });
-  } catch (error) {
-    res.status(500);
-    throw new Error("Internal Server Error");
-  }
-});
+    res.status(200).json({ message: "Reset password email sent!" });
+  })
+);
 
-//reset password
-router.post("/resetPassword", async (req, res, next) => {
-  const { otp, password } = req.body;
-  console.log(otp, password);
-  try {
+// Reset password
+router.post(
+  "/resetPassword",
+  
+  asyncHandler(async (req, res, next) => {
+    const { otp, password } = req.body;
+
     const email = await otpHandler.getStoredEmail(otp);
+
     if (!email) {
-      res.status(400);
-      throw new Error("Time lmit exceeded, try again !");
+      return res.status(400).json({ message: "Link expired, try again!" });
     }
 
     const user = await User.findOne({ email });
+
     if (!user) {
-      res.status(400);
-      throw new Error("User not found");
+      return res.status(400).json({ message: "User not found" });
     }
 
-    user.password = password;
+    user.password = await bcrypt.hash(password, 13);
     await user.save();
 
-    return res.status(200).json({ message: "Password updated successfully !" });
-  } catch (error) {
-  next();
-  }
-});
+    res.status(200).json({ message: "Password updated successfully!" });
+  })
+);
 
-//logout
+// Logout
 router.post(
   "/logout",
   asyncHandler(async (req, res, next) => {
-    console.log("Logout function called !!");
     res.cookie("jwt", "", {
       httpOnly: true,
       expires: new Date(0),
     });
-    res.status(200).json({ message: "User logged out !" });
+    res.status(200).json({ message: "User logged out!" });
   })
 );
 
-// @desc    Update user profile
-// @route   PUT /user/profile
-// @access  Private
-router.patch("/profile", async (req, res, next) => {
-  //authenticate ,
-  console.log("Update profile api called !!!");
-  const user = await User.findById(req.body._id);
-  if (user) {
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-    user.phone = req.body.phone || user.phone;
-    if (req.body.password) {
-      user.password = req.body.password;
+// Update user profile
+router.patch(
+  "/profile",
+  authenticate,
+  asyncHandler(async (req, res, next) => {
+    const { _id, name, email, phone, password } = req.body;
+
+    const user = await User.findById(_id);
+
+    if (user) {
+      user.name = name || user.name;
+      user.email = email || user.email;
+      user.phone = phone || user.phone;
+
+      if (password) {
+        user.password = await bcrypt.hash(password, 13);
+      }
+
+      const updatedUser = await user.save();
+
+      res.status(200).json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        phone: updatedUser.phone,
+        role: updatedUser.role || "user",
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
     }
-
-    const updatedUser = await user.save();
-
-    res.json({
-      _id: updatedUser._id,
-      name: updatedUser.name,
-      email: updatedUser.email,
-      phone: updatedUser.phone,
-      role: updatedUser.role || "user",
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
-  }
-});
+  })
+);
 
 module.exports = router;
--- server/src/utility/generateToken.js
+++ server/src/utility/generateToken.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken'); // Import the jsonwebtoken library
 
-const generateToken=(res,payload)=>{
+const generateToken = (res, payload) => {
+  // Generate a JWT token using the payload and a secret key
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    const token = jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:'1h'})
-    
-    res.cookie('jwt',token,{
-        httpOnly:true,
-        maxAge : 1*24*60*60*1000,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite : 'none'
-    })
-    console.log("Token set to cookie")
-}
+  // Set the JWT token in an HTTP-only cookie
+  res.cookie('jwt', token, {
+    httpOnly: true, // The cookie is accessible only by the web server
+    maxAge: 1 * 24 * 60 * 60 * 1000, // The cookie will expire in 1 day
+    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    sameSite: 'none', // Allow cross-site cookie setting
+  });
 
+  console.log("Token set to cookie");
 
+  return token; // Return the generated token
+};
 
-
-
-module.exports = generateToken;
+module.exports = generateToken; // Export the generateToken function
